b5d991dadaf39b5fcfa9a1b7453bf114bda04ecf
Kamil Purgat <kpurgat.zsp9@gmail.com>
2024-09-20 09:42:51 +0200

    Add tail variable to lcd_buffer

--- Core/Inc/lcd_buffer.h
+++ Core/Inc/lcd_buffer.h
@@ -17,11 +17,13 @@ typedef struct
 {
 	int data[BITS_ON_SCREEN];
 	int head;
+	int tail;
 }lcd_buffer_s;
 
 void lcd_buffer_init(lcd_buffer_s *buffer);
 void lcd_buffer_add_next(lcd_buffer_s *buffer, uint16_t value);
 void lcd_buffer_add_previous(lcd_buffer_s *buffer, uint16_t value);
 int lcd_buffer_read_from_head(lcd_buffer_s *buffer, int index);
+int lcd_buffer_read_from_tail(lcd_buffer_s *buffer, int index);
 
 #endif /* INC_LCD_BUFFER_H_ */
--- Core/Src/lcd_buffer.c
+++ Core/Src/lcd_buffer.c
@@ -3,6 +3,7 @@
 void lcd_buffer_init(lcd_buffer_s *buffer)
 {
 	buffer->head = BITS_ON_SCREEN - 1;
+	buffer->tail = 0;
   	for (int i = 0; i < BITS_ON_SCREEN; i++){
   		buffer->data[i] = 0;
   	}
@@ -12,17 +13,27 @@ void lcd_buffer_add_next(lcd_buffer_s *buffer, uint16_t value)
 {
     buffer->data[buffer->head] = value;
     buffer->head = (buffer->head + 1) % BITS_ON_SCREEN;
+    buffer->tail = (buffer->tail + 1) % BITS_ON_SCREEN;
 }
 
 void lcd_buffer_add_previous(lcd_buffer_s *buffer, uint16_t value)
 {
 	buffer->data[buffer->head] = value;
     buffer->head = (buffer->head - 1 + BITS_ON_SCREEN) % BITS_ON_SCREEN;
+    buffer->tail = (buffer->tail - 1 + BITS_ON_SCREEN) % BITS_ON_SCREEN;
 }
 
 int lcd_buffer_read_from_head(lcd_buffer_s *buffer, int index)
 {
 	int cnt = ( (buffer->head) + index) % BITS_ON_SCREEN;
+    int result = buffer->data[cnt];
+
+    return result;
+}
+
+int lcd_buffer_read_from_tail(lcd_buffer_s *buffer, int index)
+{
+	int cnt = ( (buffer->tail) + index) % BITS_ON_SCREEN;
     int result = buffer->data[cnt];
 
     return result;
